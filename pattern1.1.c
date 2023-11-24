d185688c1a8485cc6a8fb02ec385b2ba3b6ddba3
nidhikesharvani2 <122100892+nidhikesharvani2@users.noreply.github.com>
2023-11-23 19:06:16 -0800

    Add files via upload

---
+++ pattern1.1.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("n=");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+/*111
+   222
+   333*/
---
+++ pattern1.4.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+void main()
+{
+    int n,i,j;
+    printf("n=");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("%d",n-i+1);
+        }
+        printf("\n");
+
+    }
+    /*333
+    222
+    111*/
+}
